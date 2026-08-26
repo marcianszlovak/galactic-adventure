f13eb00f0d86b7566b73fa4e6d94b0fa9da2109b
Marcian Szlovak <marcianszlovak@gmail.com>
2026-08-26 23:07:18 +0200

    switch to class based approach

--- srv/spacefarer-service.js
+++ srv/spacefarer-service.js
@@ -1,3 +1,11 @@
 import cds from "@sap/cds";
 
-export default cds.service.impl(async () => {});
+export default class SpacefarerService extends cds.ApplicationService {
+  async init() {
+    const { Spacefarers } = this.entities;
+
+    await super.init();
+
+    console.log("SpacefarerService initialized");
+  }
+}
