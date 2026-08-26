7183b45a2583bf9dd8560805a6bf9129263d646c
Marcian Szlovak <marcianszlovak@gmail.com>
2026-08-26 23:41:55 +0200

    enhance stardust collection and wormhole navigation

--- srv/spacefarer-service.js
+++ srv/spacefarer-service.js
@@ -5,13 +5,25 @@ export default class SpacefarerService extends cds.ApplicationService {
     const { Spacefarers } = this.entities;
 
     this.before("CREATE", Spacefarers, (req) => this.onBeforeCreate(req));
-
     await super.init();
 
     console.log("SpacefarerService initialized");
   }
 
   onBeforeCreate(req) {
-    console.log("Before creating a spacefarer:", req.data);
+    console.log("Received request to create a new spacefarer:", req.data);
+
+    const { stardustCollection, wormholeNavSkill, firstName, lastName } =
+      req.data;
+
+    req.data.stardustCollection = (stardustCollection ?? 0) + 50;
+    req.data.wormholeNavSkill =
+      wormholeNavSkill != null && wormholeNavSkill >= 10
+        ? wormholeNavSkill
+        : 10;
+
+    console.log(
+      `[@Before CREATE] Prepared ${firstName} ${lastName} for launch.`,
+    );
   }
 }
