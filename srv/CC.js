653fea19661a39f573125bb17e528ce618978c00
Marcian Szlovak <marcianszlovak@gmail.com>
2026-09-23 19:38:06 +0200

    add constants file

---
+++ srv/CC.js
@@ -0,0 +1,19 @@
+export const ROLE = {
+  ADMIN: "SpacefarerAdmin",
+  OFFICER: "SpacefarerOfficer",
+};
+
+export const FIELD_CONTROL = { MANDATORY: 7, READ_ONLY: 1 };
+
+export const PLANET_WRITE_EVENTS = ["NEW", "CREATE", "PATCH", "UPDATE"];
+export const PLANET_REFRESH_EVENTS = [
+  "READ",
+  "NEW",
+  "EDIT",
+  "PATCH",
+  "CREATE",
+  "UPDATE",
+];
+
+export const MIN_WORMHOLE_SKILL = 10;
+export const SIGNUP_STARDUST_BONUS = 50;
